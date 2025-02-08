5daf258e8476f2798a65a7f17c4f2417efb0082a
Dan Lourenco <danlourenco@gmail.com>
2025-02-07 21:26:16 -0500

    add callback route

---
+++ app/pages/callback.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const { $agent } = useNuxtApp();
+
+definePageMeta({
+  middleware: [
+    function (to, from) {
+      // Custom inline middleware
+      console.log($agent);
+      console.log("Middleware", to, from);
+      navigateTo("/feed");
+    },
+   
+  ],
+});
+</script>
+<template>
+    <div>Authenticated. Redirecting...</div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,26 +5,13 @@
 <template>
   <div class="container mx-auto flex flex-col justify-center align-center h-dvh w-full">
     <div class="mt-8">
-      <h1
-        class="text-8xl text-center font-heading dropshadow text-light-blue text-stroke"
-      >
+      <h1 class="text-8xl text-center font-heading dropshadow text-light-blue text-stroke">
         BlueSky Guide
       </h1>
-   
+
       <NuxtLink to="/feed" class="text-center text-4xl text-light-blue underline">
-        Go to Feed</NuxtLink>
+        Go to Feed
+      </NuxtLink>
     </div>
-
-
-
-    <!-- <form @submit.prevent="login" class="b">
-    <fieldset
-      class="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box mx-auto mt-8"
-    >
-      <button class="btn btn-neutral mt-4" type="submit">Login</button>
-    </fieldset>
-</form> -->
-
-
   </div>
 </template>
